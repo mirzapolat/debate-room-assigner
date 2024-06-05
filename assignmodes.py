9a6349ee76c01bb9fa9a5c017fd81e0e785b6519
Mirza Polat <mirza.polat@tum.de>
2024-06-06 00:30:37 +0200

    change name

--- assignmodes.py
+++ assignmodes.py
@@ -1,10 +1,36 @@
+import random
+from improvedassignment import Room, Participant
+
 def make_top_rooms(participants):
     return []
 
 def make_proam_rooms(participants):
     return []
 
-def make_random_rooms(participants):
+def make_random_rooms(participants, amount_of_rooms):
+    random.shuffle(participants)
+    rooms = []
+
+    priority_speaker_eng = [p for p in participants if p.language == 'ENG' and p.is_speaker == True and p.is_priority == True]
+    normal_speaker_eng = [p for p in participants if p.language == 'ENG' and p.is_speaker == True and p.is_priority == False]
+
+    priority_speaker_ger = [p for p in participants if p.language == 'GER' and p.is_speaker == True and p.is_priority == True]
+    normal_speaker_ger = [p for p in participants if p.language == 'GER' and p.is_speaker == True and p.is_priority == False]
+
+    priority_judge_eng = [p for p in participants if p.language == 'ENG' and p.is_speaker == False and p.is_priority == True]
+    normal_judge_eng = [p for p in participants if p.language == 'ENG' and p.is_speaker == False and p.is_priority == False]
+
+    for r in range (1, amount_of_rooms + 1):
+        if r % 2 == 1:  # English Room first
+            if (sum(1 for p in participants if p.language == 'ENG' and p.is_speaker == True) >= 8): # At least 8 speakers
+                list = []
+                for p in participants:
+                    if p.language == 'ENG' and p.is_speaker == True:
+                        list.append(p)
+                rooms.append(Room("Room " + str(r), str(r), "RANDOM", list))
+
+
+
     return []
 
 def make_fcfs_rooms(participants):
--- improvedassignment.py
+++ improvedassignment.py
@@ -1,5 +1,4 @@
 import csv
-import random
 
 from assignmodes import make_top_rooms, make_proam_rooms, make_random_rooms, make_fcfs_rooms
 
@@ -13,10 +12,10 @@ class Participant:
         self.team_name = team_name
 
 class Room:
-    def __init__(self, name, chair, mode):
+    def __init__(self, name, chair, mode, positions):
         self.name = name
         self.chair = chair          # Chair Judge of the room
-        self.positions = []
+        self.positions = positions  # List of participants in the room
         self.mode = mode            # TOP, BOTTOM, PROAM, ALL
 
 # Loads all participants from a csv file
@@ -31,26 +30,40 @@ def load_participants(filename):
                 participants.append(participant)
     return participants
 
-def make_rooms(participants, mode):
+def make_rooms(participants, mode, amount_of_rooms):
     rooms = [];
     if mode == "TOP":
-        rooms = make_top_rooms(participants)
+        rooms = make_top_rooms(participants, amount_of_rooms)
     if mode == "PROAM":
-        rooms = make_proam_rooms(participants)
+        rooms = make_proam_rooms(participants, amount_of_rooms)
     if mode == "RANDOM":
-        rooms = make_random_rooms(participants)
+        rooms = make_random_rooms(participants, amount_of_rooms)
     if mode == "FCFS":
-        rooms = make_fcfs_rooms(participants)
+        rooms = make_fcfs_rooms(participants, amount_of_rooms)
     return rooms
 
 def save_rooms(rooms, filename):
     print("Saving rooms to " + filename)
 
 def main():
-    participants = load_participants(input("Participants CSV File:              "))
-    amout_of_rooms = int(input("Amount of rooms:                    "))
-    rooms = make_rooms(participants, input("Mode (TOP, PROAM, RANDOM, FCFS):    "))
-    save_rooms(rooms, input("Output CSV File:                    "))
+    participants = load_participants(input("Participants File: "))
+    while len(participants) == 0 or participants == None:    # Check if participants are loaded
+        print("<!> No participants found in file")
+        participants = load_participants(input("Participants File: "))
+
+    amout_of_rooms = int(input("Amount of rooms: "))
+    while amout_of_rooms < 1:   # Check if amount of rooms is greater than 0
+        print("<!> Amount of rooms must be greater than 0")
+        amout_of_rooms = int(input("Amount of rooms: "))
+
+    roommode = input("Mode (TOP, PROAM, RANDOM, FCFS): ")
+    while roommode not in ["TOP", "PROAM", "RANDOM", "FCFS"]:   # Check if mode is valid
+        print("<!> Mode must be TOP, PROAM, RANDOM or FCFS")
+        roommode = input("Mode: ")
+
+    rooms = make_rooms(participants, roommode, amout_of_rooms)
+
+    save_rooms(rooms, input("Output File: "))
     print("... Completed!")
 
 if __name__ == "__main__":
